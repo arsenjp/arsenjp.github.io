ae281eec90f6ccb3f40e3b7efeba57115b789f2f
arsen <arsenjp24@gmail.com>
2022-11-24 11:38:38 +0900

    some changes

---
+++ _nuxt/static/1669257513/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fblogs\u002Fcareer","\u002F","\u002Fblogs\u002Flife","\u002Fblogs\u002Fmiscellaneous","\u002Fleetcode","\u002Fpassword-reset","\u002Fblogs\u002Fproject","\u002Fblogs\u002Fprogramming","\u002Fblogs\u002Ftechnology","\u002Finspire","\u002Fsignup","\u002Fblogs\u002Fresearch","\u002Flogin","\u002Fblogs\u002Fbusiness"]})
