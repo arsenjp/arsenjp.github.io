b450c2b6849851f3ae379838c6180633bb7a15df
arsen <arsenjp24@gmail.com>
2022-12-05 18:10:50 +0900

    some changes

--- _nuxt/static/1670203345/admin/codes/code/new/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670203345",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin\u002Fcodes\u002Fcode\u002Fnew",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
